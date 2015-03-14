3c713d19980aed1db791a236736d5961bd03cab0
Ryan Thompson <service@aiwebsystems.com>
2015-03-14 16:06:57 -0500

    SP

--- src/LocalizationModuleServiceProvider.php
+++ src/LocalizationModuleServiceProvider.php
@@ -14,89 +14,62 @@ class LocalizationModuleServiceProvider extends ServiceProvider
 {
 
     /**
-     * Boot the service provider.
+     * The addon plugins.
+     *
+     * @var array
      */
-    public function boot()
-    {
-        if (env('INSTALLED')) {
-            $this->app->make('twig')->addExtension(
-                $this->app->make('Anomaly\LocalizationModule\Language\LanguagePlugin')
-            );
-        }
-    }
+    protected $plugins = [
+        'Anomaly\LocalizationModule\Language\LanguagePlugin'
+    ];
 
     /**
-     * Register the service provider.
+     * The class bindings.
      *
-     * @return void
+     * @var array
      */
-    public function register()
-    {
-        /**
-         * Country registration.
-         */
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Country\CountryModel',
-            'Anomaly\LocalizationModule\Country\CountryModel'
-        );
+    protected $bindings = [
+        'Anomaly\LocalizationModule\Country\CountryModel'   => 'Anomaly\LocalizationModule\Country\CountryModel',
+        'Anomaly\LocalizationModule\Currency\CurrencyModel' => 'Anomaly\LocalizationModule\Currency\CurrencyModel',
+        'Anomaly\LocalizationModule\Language\LanguageModel' => 'Anomaly\LocalizationModule\Language\LanguageModel',
+        'Anomaly\LocalizationModule\State\StateModel'       => 'Anomaly\LocalizationModule\State\StateModel',
+        'Anomaly\LocalizationModule\Zone\ZoneModel'         => 'Anomaly\LocalizationModule\Zone\ZoneModel'
+    ];
 
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Country\Contract\CountryRepositoryInterface',
-            'Anomaly\LocalizationModule\Country\CountryRepository'
-        );
+    /**
+     * The singleton bindings.
+     *
+     * @var array
+     */
+    protected $singletons = [
+        'Anomaly\LocalizationModule\Country\Contract\CountryRepositoryInterface'   => 'Anomaly\LocalizationModule\Country\CountryRepository',
+        'Anomaly\LocalizationModule\Currency\Contract\CurrencyRepositoryInterface' => 'Anomaly\LocalizationModule\Currency\CurrencyRepository',
+        'Anomaly\LocalizationModule\Language\Contract\LanguageRepositoryInterface' => 'Anomaly\LocalizationModule\Language\LanguageRepository',
+        'Anomaly\LocalizationModule\State\Contract\StateRepositoryInterface'       => 'Anomaly\LocalizationModule\State\StateRepository',
+        'Anomaly\LocalizationModule\Zone\Contract\ZoneRepositoryInterface'         => 'Anomaly\LocalizationModule\Zone\ZoneRepository'
+    ];
 
-        /**
-         * Currency registration.
-         */
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Currency\CurrencyModel',
-            'Anomaly\LocalizationModule\Currency\CurrencyModel'
-        );
+    /**
+     * The addon routes.
+     *
+     * @var array
+     */
+    protected $routes = [
+        'admin/localization'                      => 'Anomaly\LocalizationModule\Http\Controller\Admin\LanguagesController@redirect',
+        'admin/localization/countries'            => 'Anomaly\LocalizationModule\Http\Controller\Admin\CountriesController@index',
+        'admin/localization/countries/create'     => 'Anomaly\LocalizationModule\Http\Controller\Admin\CountriesController@create',
+        'admin/localization/countries/edit/{id}'  => 'Anomaly\LocalizationModule\Http\Controller\Admin\CountriesController@edit',
+        'admin/localization/currencies'           => 'Anomaly\LocalizationModule\Http\Controller\Admin\CurrenciesController@index',
+        'admin/localization/currencies/create'    => 'Anomaly\LocalizationModule\Http\Controller\Admin\CurrenciesController@create',
+        'admin/localization/currencies/edit/{id}' => 'Anomaly\LocalizationModule\Http\Controller\Admin\CurrenciesController@edit',
+        'admin/localization/languages'            => 'Anomaly\LocalizationModule\Http\Controller\Admin\LanguagesController@index',
+        'admin/localization/languages/create'     => 'Anomaly\LocalizationModule\Http\Controller\Admin\LanguagesController@create',
+        'admin/localization/languages/edit/{id}'  => 'Anomaly\LocalizationModule\Http\Controller\Admin\LanguagesController@edit',
+        'admin/localization/states'               => 'Anomaly\LocalizationModule\Http\Controller\Admin\StatesController@index',
+        'admin/localization/states/create'        => 'Anomaly\LocalizationModule\Http\Controller\Admin\StatesController@create',
+        'admin/localization/states/edit/{id}'     => 'Anomaly\LocalizationModule\Http\Controller\Admin\StatesController@edit',
+        'admin/localization/zones'                => 'Anomaly\LocalizationModule\Http\Controller\Admin\ZonesController@index',
+        'admin/localization/zones/create'         => 'Anomaly\LocalizationModule\Http\Controller\Admin\ZonesController@create',
+        'admin/localization/zones/edit/{id}'      => 'Anomaly\LocalizationModule\Http\Controller\Admin\ZonesController@edit'
+    ];
 
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Currency\Contract\CurrencyRepositoryInterface',
-            'Anomaly\LocalizationModule\Currency\CurrencyRepository'
-        );
-
-        /**
-         * Language registration.
-         */
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Language\LanguageModel',
-            'Anomaly\LocalizationModule\Language\LanguageModel'
-        );
-
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Language\Contract\LanguageRepositoryInterface',
-            'Anomaly\LocalizationModule\Language\LanguageRepository'
-        );
-
-        /**
-         * State registration.
-         */
-        $this->app->bind(
-            'Anomaly\LocalizationModule\State\StateModel',
-            'Anomaly\LocalizationModule\State\StateModel'
-        );
-
-        $this->app->bind(
-            'Anomaly\LocalizationModule\State\Contract\StateRepositoryInterface',
-            'Anomaly\LocalizationModule\State\StateRepository'
-        );
-
-        /**
-         * Zone registration.
-         */
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Zone\ZoneModel',
-            'Anomaly\LocalizationModule\Zone\ZoneModel'
-        );
-
-        $this->app->bind(
-            'Anomaly\LocalizationModule\Zone\Contract\ZoneRepositoryInterface',
-            'Anomaly\LocalizationModule\Zone\ZoneRepository'
-        );
-
-        $this->app->register('Anomaly\LocalizationModule\LocalizationModuleRouteProvider');
-    }
 }
