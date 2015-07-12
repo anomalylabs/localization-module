99d4893742e4395bd41cdfde2b4a9147148999ef
MarceauKa <marceau@casals.fr>
2015-07-12 18:31:19 +0200

    Ajout de la traduction française

---
+++ resources/lang/fr/addon.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name'        => 'Localisation',
+    'description' => 'Gestion des localisations.',
+    'section'     => [
+        'currencies' => 'Monnaies',
+        'languages'  => 'Langues',
+        'countries'  => 'Pays',
+        'states'     => 'Etats',
+        'zones'      => 'Zones'
+    ]
+];
---
+++ resources/lang/fr/field.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'name'            => [
+        'name' => 'Nom'
+    ],
+    'iso'             => [
+        'name' => 'Code ISO'
+    ],
+    'date_format'     => [
+        'name' => 'Format de date'
+    ],
+    'datetime_format' => [
+        'name' => 'Format de date et heure'
+    ],
+    'enabled'         => [
+        'name' => 'Activé'
+    ],
+    'rtl'             => [
+        'name'  => 'RTL',
+        'label' => 'Est-ce un langage droite vers gauche ?'
+    ],
+    'flag'            => [
+        'name' => 'Drapeau'
+    ],
+    'call_prefix'     => [
+        'name' => 'Préfixe téléphonique'
+    ],
+    'address_format'  => [
+        'name' => 'Format d\'adresse'
+    ],
+    'numeric_iso'     => [
+        'name' => 'Code ISO numérique'
+    ],
+    'symbol'          => [
+        'name' => 'Symbole'
+    ],
+    'exchange_rate'   => [
+        'name' => 'Taux de change'
+    ],
+    'zone'            => [
+        'name' => 'Zone'
+    ],
+    'currency'        => [
+        'name' => 'Monnaie'
+    ],
+    'country'         => [
+        'name' => 'Pays'
+    ]
+];
