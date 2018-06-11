5468c8f576367edfa4bd5ed89c4dbe5121f85f9b
Cyril de Wit <cyrilwit@gmail.com>
2018-06-11 23:06:32 +0200

    Add dutch translations for button.php

---
+++ resources/lang/nl/button.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'new_language' => 'Nieuwe Taal',
+    'new_zone' => 'Nieuwe Zone',
+    'new_country' => 'Nieuw Land',
+    'new_state' => 'Nieuwe Staat',
+    'new_currency' => 'Nieuwe Valuta',
+];
