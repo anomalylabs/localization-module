f91f22cdaa3986f09e972f3503a5c4f63503157a
Cyril de Wit <cyrilwit@gmail.com>
2018-06-11 23:05:14 +0200

    Add dutch translations for addon.php

---
+++ resources/lang/nl/addon.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name'        => 'Lokalisatie',
+    'description' => 'Beheer van systeemlokalisatie.',
+    'section'     => [
+        'currencies' => 'Valuta',
+        'languages'  => 'Talen',
+        'countries'  => 'Landen',
+        'states'     => 'Staten',
+        'zones'      => 'Zones'
+    ]
+];
